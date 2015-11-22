95990e3a1d8590b031d7ed7b05397af8e608f8da
Quentin <mrtchiki@gmail.com>
2015-11-22 15:06:57 +0100

    On compte les Happy

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 import java.lang.System;
 
-
 class Main {
     public static void main(String[] args) {
         System.out.println("Hey I'm the main ! :)");
--- TwitterStreamConsumer.java
+++ TwitterStreamConsumer.java
@@ -3,6 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.String;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+ 
 import org.scribe.builder.*;
 import org.scribe.builder.api.*;
 import org.scribe.model.*;
@@ -11,11 +16,12 @@ import org.scribe.oauth.*;
 
 public class TwitterStreamConsumer extends Thread {
 
-    private static String CONSUMER_KEY = "xpRa8FuS4TvLdIuQmEZXaae6N";
-    private static String CONSUMER_SECRET = "ccE6lFnzirvSjBttgzofyHyOnVkwF5aKQwK29qJBgpo0IoZHgz";
+    private static String CONSUMER_KEY = "0bQkZcnF0VGHrvJv6y55Yhuc7";//"xpRa8FuS4TvLdIuQmEZXaae6N";
+    private static String CONSUMER_SECRET = "JfIkF3wNWul9SVpIb8cKpeKZt2TKrLiDgigVLIhYolACvo5TAm";//"ccE6lFnzirvSjBttgzofyHyOnVkwF5aKQwK29qJBgpo0IoZHgz";
 
 
     private static final String STREAM_URI = "https://stream.twitter.com/1.1/statuses/filter.json";
+
 
     public void run(){
         try{
@@ -38,7 +44,7 @@ public class TwitterStreamConsumer extends Thread {
             request.addHeader("host", "stream.twitter.com");
             request.setConnectionKeepAlive(true);
             request.addHeader("user-agent", "Twitter Stream Reader");
-            request.addBodyParameter("track", "java,heroku,twitter"); // Set keywords you'd like to track here
+            request.addBodyParameter("track", "happy,satisfied,joyful,joyous,chherful,contented,delighted,ecstatic,depressed,disturbed,sad,sadness,upset,unhappy,troubled,disappointed"); // Set keywords you'd like to track here
             service.signRequest(accessToken, request);
             Response response = request.send();
 
@@ -46,9 +52,17 @@ public class TwitterStreamConsumer extends Thread {
             BufferedReader reader = new BufferedReader(new InputStreamReader(response.getStream()));
 
             String line;
+
+            // Création du fichier texte pour le programme
+		        File fichierTexte = new File ("res.json");
+		        // Création de "l'écrivain"
+		        FileWriter ecrireFichier;
+            // Instanciation de l'objet ecrireFichier qui va écrire dans fichierTexte.txt
+			      ecrireFichier = new FileWriter(fichierTexte);
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+              ecrireFichier.write(line);
             }
+
         }
         catch (IOException ioe){
             ioe.printStackTrace();

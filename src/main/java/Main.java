f9e47a0a5c7a70540c188b65e677caab7626ab0c
Antoine Muller <nemak121@gmail.com>
2015-12-12 16:54:06 +0100

    Add gradle

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+package tweetreduce;
+import java.lang.System;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hey I'm the main ! :)");
+
+        final TwitterStreamConsumer streamConsumer = new TwitterStreamConsumer();
+        streamConsumer.start();
+
+    }
+}
